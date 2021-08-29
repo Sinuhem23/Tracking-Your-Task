dab534a83c6c6b64b5d7735a1fd4eea293deeb72
sinuhem23 <sinuhem23@gmail.com>
2021-08-29 00:33:09 -0400

    forming task layout and functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Aug 28th at 10:00am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Job Meeting',
+      day: 'Aug 30th at 9:00am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'The Gym',
+      day: 'Aug 29th at 7:00am',
+      reminder: false,
+    },
+  ]);
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,13 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Aug 28th at 10:00am',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Job Meeting',
-    day: 'Aug 30th at 9:00am',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'The Gym',
-    day: 'Aug 29th at 7:00am',
-    reminder: false,
-  },
-];
+import Task from './Task';
 
-const Tasks = () => {
-  return <div></div>;
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
